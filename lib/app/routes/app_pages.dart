5b206714e4939e5d8d6771f24431bea68a076e99
rafaelgimenes <rafaelrgimenes@gmail.com>
2026-04-04 17:54:37 -0300

    feat: tela de favoritos e integracao de rotas finalizada

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,11 +3,13 @@ import 'app_routes.dart';
 import '../../features/auth/view/login_page.dart';
 import '../../features/auth/view/register_page.dart';
 import '../../features/splash/view/splash_page.dart';
+import '../../features/favorites/view/favorites_page.dart';
 
 abstract class AppPages {
   static Map<String, WidgetBuilder> get routes => {
     AppRoutes.login: (_) => const LoginPage(),
     AppRoutes.register: (_) => const RegisterPage(),
     AppRoutes.splash: (_) => const SplashPage(),
+    AppRoutes.favorites: (_) => const FavoritesPage(),
   };
 }
---
+++ lib/features/favorites/viewmodel/favorites_viewmodel.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class FavoritesViewmodel extends ChangeNotifier {
+  bool isLoading = false;
+
+  // No futuro, a lógica de carregar a lista do histórico/favoritos virá aqui
+}
